10398ab25c0269ed07381bb6e835d0f60b61c04a
sivaperumal64 <164004904+sivaperumal64@users.noreply.github.com>
2024-04-25 16:23:24 +0530

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,6 @@
+function createHeading() {
+  var container = document.getElementById('container');
+  var heading = document.createElement('h1');
+  heading.textContent = 'Hello World';
+  container.appendChild(heading);
+}
